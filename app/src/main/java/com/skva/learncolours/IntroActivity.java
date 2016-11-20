30ac824572ce9d4f8a511b873b557f5387d25bfc
Kartik Narayanan <kartik.narayanan@gmail.com>
2016-11-20 12:01:44 +0530

    added intro slides

--- app/src/main/java/com/skva/learncolours/IntroActivity.java
+++ app/src/main/java/com/skva/learncolours/IntroActivity.java
@@ -27,12 +27,15 @@ public class IntroActivity extends AppIntro {
 
         // Instead of fragments, you can also use our default slide
         // Just set a title, description, background and image. AppIntro will do the rest.
-        addSlide(AppIntroFragment.newInstance("Learn By Listening", "blaps", R.drawable.fruits_apple, Color.parseColor("#6CD3F9")));
-        addSlide(AppIntroFragment.newInstance("kundi", "bum bum", R.drawable.help, Color.parseColor("#6CD3F9")));
+        addSlide(AppIntroFragment.newInstance("Learn to Identify", "Welcome! We built this game to help you teach your toddler new words ", R.drawable.applauncher, Color.parseColor("#0f1b07")));
+        addSlide(AppIntroFragment.newInstance("How To Use", "Touch the categories to show their associated objects", R.drawable.help, Color.parseColor("#0f1b07")));
+        addSlide(AppIntroFragment.newInstance("How To Use", "Learn Mode: Touch objects to hear their names spoken out." + '\n' + " Play mode: Turn speech off and ask your child to choose objects.", R.drawable.help, Color.parseColor("#0f1b07")));
+
+        addSlide(AppIntroFragment.newInstance("Things to Know", "This game uses Google's Text to Speech engine. This might take upto 10 seconds to get ready. We appreciate your patience.", R.drawable.applauncher, Color.parseColor("#0f1b07")));
         // OPTIONAL METHODS
         // Override bar/separator color.
-        setBarColor(Color.parseColor("#026D0F"));
-        setSeparatorColor(Color.parseColor("#026D0F"));
+        setBarColor(Color.parseColor("#00C853"));
+        setSeparatorColor(Color.parseColor("#4cb5f5"));
 
         // Hide Skip/Done button.
         showSkipButton(true);
